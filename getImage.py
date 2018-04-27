19d5c041a2f1234d8c143c952fb62926c1b6e19b
bokyoung <insecurity22@naver.com>
2018-04-27 14:03:23 +0900

    Update getImage.py

--- getImage.py
+++ getImage.py
@@ -1,29 +1,38 @@
 import sys
-import base64
 import requests
 from bs4 import BeautifulSoup
+import re
+import base64
 
 def help():
     print('Usage: getImage [url]')
 
-def download_web_image(url):
+# regex = Regular expression
+def remove_tag(content):
+    regex = re.compile('\[<.*?>|<.*?>\]') # pattern object
+    cleantext = regex.sub('', str(content))
+    return cleantext
 
+def find_url(content):
+    regex = re.compile('^(https?:\/\/)([0-9a-z\.-]+).([a-z]\/)') # http:// or https://
+    text = regex.findall(str(content))
+    print(text)
+
+def download_web_image(url):
     resp = requests.get(url)
 
     is_ok = resp.ok  # HTTP (True/False)
     if(is_ok == 1):
-        html = resp.text    # html source
-        # print(html)
-        # < Compare >
-        # F12 Debug mode
-        # Parsing html
-        # == because two codes can different
-
+        html = resp.text    # Check print(html) = html source
         soup = BeautifulSoup(html, 'html.parser')    # interpret html
-        tags = soup.select('#tooncontentdata')       # parsing html tag
-        # base 64 Encoding -> base 64 Decoding
-        print(str(base64.b64decode(tags)))
+        tags = soup.select('#tooncontentdata')       # id = tooncontentdata
+        # This part was base 64 Encoding -> base 64 Decoding
 
+        decodetags = base64.b64decode(remove_tag(tags))
+        remove_tag(decodetags)
+        print(decodetags)
+        print("\n\n")
+        find_url(decodetags)
     else:
         print("Can't connect\n")
 
