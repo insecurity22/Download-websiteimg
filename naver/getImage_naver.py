424e24d49df7c013c330f6565dada04681b37ede
insecurity22 <insecurity22@naver.com>
2021-02-27 01:18:35 +0900

    Update getImage_naver.py

--- naver/getImage_naver.py
+++ naver/getImage_naver.py
@@ -1,71 +1,63 @@
-﻿
 import sys
 import requests
 from bs4 import BeautifulSoup
 import re
-import time
 import os
+import time
 
-def help():
-    #
-    # ./getImage url save_path
-    #   save_path = C:\Webtoon_name\17화
-    #
-    print('Usage: ./getImage url save_path')
-    sys.exit(1)
+def findImgSrc(tag):
+    return re.compile('https:\/\/image.*\.jpg').findall(str(tag))
 
-def create_folder(save_path):
-    if os.path.isdir(save_path) == False:
-        print("Creating new folder...\n")
-        os.makedirs(save_path)
+def createFolder(savepath):
+    if os.path.isdir(savepath) == False:
+        print("Creating new folder ...\n")
+        os.makedirs(savepath)
 
-def find_img_url_to_regex(content):
-    regex = re.compile('https:\/\/image.*\.jpg')
-    return regex.findall(str(content))
-
-def naming(num):
-    # save_path + number(ex: 1) + ".jpg"
-    name = sys.argv[2] + "\\" + str(num) + ".jpg"
-    print(name)
-    return name
-
-def download_webtoon(url):
+def download(url, savepath):
     resp = requests.get(url)
-    is_ok = resp.ok
-    if(is_ok == True):
-        html = resp.text # html source
-        soup = BeautifulSoup(html, 'html.parser') # for parsing
+    if(resp.ok == True):
+        html = resp.text
+        soup = BeautifulSoup(html, 'html.parser')
 
-        # After img tag id find, all image download
         id_num = 0
-        hdr = {'User-Agent':'Mozilla/5.0', 'referer':'http://m.naver.com'}
+        header = {'User-Agent':'Mozilla/5.0', 'Referer':'http://m.naver.com'}
+        # User-Agent : 브라우저 종류
+        # Referer : 이전 페이지 URL
+        # Accept-Language : 어떤 언어의 응답을 원하는가
+        # Authorization : 인증 정보
+        # https://goodthings4me.tistory.com/140
+
         while True:
             try:
-                # Find
-                img_tag_id = "#content_image_" + str(id_num)
-                select = soup.select(img_tag_id) # ex) <img src="https:// ...
-                regex = find_img_url_to_regex(select) # https:// ...
-                print(regex)
+                # Get img src
+                img_id_tag = '#content_image_' + str(id_num)
+                img_tag = soup.select(img_id_tag) # <img src="https:// ...
+                img_src = findImgSrc(img_tag)
+                print(img_src)
+
+                filename = savepath + "\\" + str(id_num) + re.compile('(.png|.jpg|jpeg|.gif)').search(img_src[0]).group()
+                print(filename + "에 저장 중...")
 
                 # Download
-                img = requests.get(regex[0], headers=hdr).content
-                print(img) # response content is binary ex) b'\xff\xd8 ...
-
-                filename = naming(id_num+1)
+                img = requests.get(img_src[0], headers=header).content # ex) b'\xff\xd8 ...
                 with open(filename, 'wb') as f:
                     f.write(img)
+
                 id_num += 1
                 time.sleep(2)
 
             except Exception as ex:
                 break
 
-if(len(sys.argv)!=3): # Usage
-    help()
 
-# Check input value
-print("Your input value is ...\nargv[1] = ", sys.argv[1])
-print("argv[2] = ", sys.argv[2], "\n")
+if __name__ == '__main__':
 
-create_folder(sys.argv[2])
-download_webtoon(sys.argv[1])
+    if len(sys.argv) != 3:
+        print('Usage: ./getImage url save_path')
+        sys.exit(1)
+
+    print("Parameter is ...")
+    print('argv[1] : ', sys.argv[1])
+    print('argv[2] : ', sys.argv[2], "\n")
+
+    download(sys.argv[1], sys.argv[2])
