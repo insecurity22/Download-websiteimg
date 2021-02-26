e42fc45f86a8892f18194f250c1aa6a8c71c74af
insecurity22 <insecurity22@naver.com>
2021-02-27 03:49:57 +0900

    Update getImage_naver_all_episode.py

--- naver/getImage_naver_all_episode.py
+++ naver/getImage_naver_all_episode.py
@@ -1,101 +1,85 @@
-﻿
 import sys
 import requests
 from bs4 import BeautifulSoup
 import re
-import time
 import os
+import time
 
-def help():
-    #
-    # ./getImage url save_path episode_number
-    # url = https://...
-    # save_path = C:\Webtoon_name\17화
-    # episode_number = 17
-    #
-    print('Usage: ./getImage url save_path episode_number')
-    sys.exit(1)
+def findImgSrc(tag):
+    return re.compile('https:\/\/image.*\.jpg').findall(str(tag))
 
-def create_folder(save_path):
-    if os.path.isdir(save_path) == False:
-        print("Creating new folder...\n")
-        os.makedirs(save_path)
+def createFolder(savepath):
+    if os.path.isdir(savepath) == False:
+        print("Creating new folder ...\n")
+        os.makedirs(savepath)
 
-def find_img_url_to_regex(content):
-    regex = re.compile('https:\/\/image.*\.jpg')
-    return regex.findall(str(content))
-
-def naming(num):
-    # save_path + number(ex: 1) + ".jpg"
-    name = sys.argv[2] + "\\" + str(num) + ".jpg"
-    print(name)
-    return name
-
-def download_webtoon(url):
+def download(url, savepath, episodenum):
     resp = requests.get(url)
-    is_ok = resp.ok
-    if(is_ok == True):
-        html = resp.text # html source
-        soup = BeautifulSoup(html, 'html.parser') # for parsing
+    if(resp.ok == True):
+        html = resp.text
+        soup = BeautifulSoup(html, 'html.parser')
 
-        # After img tag id find, all image download
         id_num = 0
-        hdr = {'User-Agent':'Mozilla/5.0', 'referer':'http://m.naver.com'}
+        header = {'User-Agent':'Mozilla/5.0', 'Referer':'http://m.naver.com'}
+        # User-Agent : 브라우저 종류
+        # Referer : 이전 페이지 URL
+        # Accept-Language : 어떤 언어의 응답을 원하는가
+        # Authorization : 인증 정보
+        # https://goodthings4me.tistory.com/140
+
         while True:
             try:
-                # Find
-                img_tag_id = "#content_image_" + str(id_num)
-                select = soup.select(img_tag_id) # ex) <img src="https:// ...
-                regex = find_img_url_to_regex(select) # https:// ...
-                print(regex)
+                # Get img src
+                img_id_tag = '#content_image_' + str(id_num)
+                img_tag = soup.select(img_id_tag) # <img src="https:// ...
+                img_src = findImgSrc(img_tag)
+                print(img_src, "has been downloaded")
 
                 # Download
-                img = requests.get(regex[0], headers=hdr).content
-                print(img) # response content is binary ex) b'\xff\xd8 ...
-
-                filename = naming(id_num+1)
+                filename = savepath + str(id_num) + re.compile('(.png|.jpg|jpeg|.gif)').search(img_src[0]).group()
+                img = requests.get(img_src[0], headers=header).content # ex) b'\xff\xd8 ...
                 with open(filename, 'wb') as f:
                     f.write(img)
+
                 id_num += 1
                 time.sleep(2)
 
-            except Exception as ex:
-                return regex
+            except Exception as e:
                 break
 
-def change_url_and_folder_path(url, path):
-    global episode
-    episode += 1
+if __name__ == '__main__':
 
-    # Change URL
-    no = "no=" + str(episode) + "&"
-    url_text = re.sub(r'no=.*&', no, url)
-    print(url_text)
+    if len(sys.argv) != 4:
+        print('Usage: ./getImage url save_path episode_number')
+        print('url : Webtoon URL')
+        print('save_path : 다운 받을 이미지를 저장할 경로')
+        print('episode_number : 1')
+        sys.exit(1)
 
-    # Change save path
-    text = str(episode) + "화"
-    path_text = re.sub(r'[0-9]*화', text, path)  # 패턴(text)과 일치하는 문자열(path중) 변경
-    print(path_text)
-    return (url_text, path_text)
+    print("Parameter is ...")
+    print('argv[1] : ', sys.argv[1])
+    print('argv[2] : ', sys.argv[2])
+    print('argv[3] : ', sys.argv[3], "\n")
 
-if(len(sys.argv)!=4): # Usage
-    help()
+    url = sys.argv[1]
+    savepath = sys.argv[2]
+    episodenum = int(sys.argv[3])
+    while True:
+        try:
+            # Create folder
+            folder = savepath + "\\" + str(episodenum) + "\\"
+            createFolder(folder)
+            print(folder, "폴더 생성")
 
-# Check input value
-print("Your input value is ...\nargv[1] = ", sys.argv[1])
-print("argv[2] = ", sys.argv[2])
-print("argv[3] = ", sys.argv[3], "\n")
+            print(str(episodenum) + "화 다운로드 시작")
+            download(url, folder, episodenum)
 
-episode = int(sys.argv[3])
+            # Change URL
+            episodenum += 1
+            no = "no=" + str(episodenum)
+            url = url.replace(re.compile("no=\d*").search(url).group(), no)
 
-while True:
-    try:
-        create_folder(sys.argv[2])
-        checklist = download_webtoon(sys.argv[1])
-        print("Download complete", episode, "화")
-        (sys.argv[1], sys.argv[2]) = change_url_and_folder_path(sys.argv[1], sys.argv[2])
-    except Exception as ex:
-        if not checklist: # If List is empty
-            continue
-        if ex:
+            time.sleep(5)
+        except Exception as e:
+            print(e)
             break
